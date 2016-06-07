78a012ec41d3f14b37d63af412277ded3a75dcd2
StaubOliver <staub.olivier@gmail.com>
2016-06-07 21:56:17 +0100

    modal

--- application/views/map2.php
+++ application/views/map2.php
@@ -95,11 +95,11 @@
 				
 
 				<!-- Modal to show large pictures -->
-				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-large-image fade" id="Modal-large-image" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 					<div class="modal-dialog modal-lg" role="document">
 						<div class="modal-content">
 							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<button type="button" class="close" data-dismiss="modal-large-image" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 								<h4 class="modal-title" id="modal-image-title"></h4>
 							</div>
 							<div class="modal-body" id="modal-image-body">
