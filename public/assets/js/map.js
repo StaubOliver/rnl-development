d5b1b59a6e42c4bc0d854f961cb345c2d06fe372
StaubOliver <staub.olivier@gmail.com>
2016-06-07 22:03:18 +0100

    link

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -125,7 +125,7 @@ function refresh(http)
 								+ "<p class='infowindow-text'> <strong> Location : </strong>" + item["place"] + " " + item["country"] + "</p>"
 							+ "</div>"
 							+ "<div class='col-xs-12'>"
-								+ "<p class='infowindow-text'>Wrong spot ?</p>"
+								+ "<a class='infowindow-text' href='#'>Wrong spot ?</a>"
 							+ "</div>"
 						+"</div>"
 
