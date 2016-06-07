94d0dfb79d37f55a3f09599a73746aa8b591ad3e
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-07 20:20:17 +0100

    remove save button

--- application/views/map.php
+++ application/views/map.php
@@ -98,11 +98,9 @@
 						<h4 class="modal-title" id="myModalLabel">Modal title</h4>
 					</div>
 					<div class="modal-body" id="modal-image-body">
-						
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
 					</div>
 				</div>
 			</div>
