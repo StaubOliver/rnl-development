d098510cc5c3387dabda0c6c1bcd518c79533499
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-07 19:04:21 +0100

    map

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -140,19 +140,23 @@ var map = angular.module('map', [])
 	
 	actualmap.addListener("click", function(){
 		console.log('map clicked');
+		logActivity(http, "Map click", user_id);
 	});
 
 	actualmap.addListener("dragend", function(){
 		console.log('map dragged');
+		logActivity(http, "Map drag", user_id);
 	});
 
 	actualmap.addListener("zoom_changed", function(){
 		new_zoom = actualmap.getZoom();
 		if (new_zoom>map_zoom){
-			console.log("zoom out");
+			console.log("zoom in");
+			logActivity(http, "Map zoom in", user_id);
 		}
 		else {
-			console.log("zoom in");
+			console.log("zoom out");
+			logActivity(http, "Map zoom out", user_id);
 		}
 		map_zoom=new_zoom;
 	});
