c4bb87f7f04a8cb1b8cc212d609df26c0d6e4584
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-07 21:44:08 +0100

    css

--- application/views/map2.php
+++ application/views/map2.php
@@ -112,13 +112,9 @@
 			<h2 >Loading...</h2>
 		</div>
 		-->
-			<div class="row">
-				<div class="col-md-12">
-					<div class="map-section">
-						<div id="googleMap"></div>
-					</div>
-				</div>
-			</div>
+		<div class="map-section">
+			<div id="googleMap"></div>
+		</div>
 	</div>
 
 	<!-- Filter  & Feedback -->
