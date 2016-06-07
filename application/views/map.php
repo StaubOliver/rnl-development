f415001fa364f4952e2d6ab2a25944efbfb4be8a
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-07 16:36:06 +0100

    debug

--- application/views/map.php
+++ application/views/map.php
@@ -56,7 +56,7 @@
 <body>
 	<!-- Header -->
 	<nav class="navbar navbar-inverse navbar-fixed-top" >
-		<div class="container-fluid">
+		<div ng-controller='map-navbar' class="container-fluid">
 
 			<div class="navbar-header">
 				<a class="navbar-brand" href="#">Map</a>
