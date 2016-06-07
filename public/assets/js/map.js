553602dae70ccc7d4117cc46e21cd82b481a01de
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-07 20:06:22 +0100

    modal image

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -38,6 +38,7 @@ function createMarkers(info, http){
 		infoWindow.close;
 		infoWindow.setContent(info["content"]);
 		logActivity(http, "Click on fossil "+info['id']+" "+info['title'], user_id);
+		document.getElementById('modal-image-body').innerHTML = "<div><img data-toggle='modal' src='"+item["url"]+"></div>";
 		infoWindow.open('actualmap', marker);
 	});
 
