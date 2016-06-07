87d1bfe00caa714aa04ff68d2b32dd489876e1a6
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-07 17:10:25 +0100

    debug

--- application/controllers/Map.php
+++ application/controllers/Map.php
@@ -33,12 +33,12 @@ class map extends CI_Controller {
 			$data['projects'] = $this->MapModel->loadProject();
 			$data['genuses'] = $this->MapModel->loadGenuses();
 			$data['collectors'] = $this->MapModel->loadCollector();
-			$data['logged_in'] = false;
 
 			if ($this->ion_auth->logged_in()){
 				$data['logged_in'] = true;
 				$data['is_admin'] = $this->ProfileModel->isAdmin();
 			} else{
+				$data['logged_in'] = false;
 				$data['is_admin'] = "0";
 			}
 
