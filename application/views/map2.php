24a072db73796c527d474e60a3262af6891fab94
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-08 18:42:37 +0100

    css

--- application/views/map2.php
+++ application/views/map2.php
@@ -332,7 +332,9 @@
 									<div class="col-md-12">
 										<p>You can select fossils on the map:</p>
 										<div ng-repeat='marker in selected_markers'>
-											<div class='btn btn-default btn-xs' ng-click="remove_marker(marker, $index)" ng-mouseover="highlight_marker(marker)" ng-mouseleave="remove_highlight(marker)"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>{{ marker['title']}}
+											</div class="col-md-4">
+												<div class='btn btn-default btn-xs' ng-click="remove_marker(marker, $index)" ng-mouseover="highlight_marker(marker)" ng-mouseleave="remove_highlight(marker)"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>{{ marker['title']}}
+												</div>
 											</div>
 										</div>
 									</div>
