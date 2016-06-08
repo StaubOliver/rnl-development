6d9d75a8cc8fd7bb87792a47629292dd63fff21d
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-08 13:57:00 +0100

    js refactor

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -3,230 +3,230 @@ var map = angular.module('map', [])
 	
 	var actualmap;
 
-var markers = [];
-var selected_markers = [];
+	var markers = [];
+	var selected_markers = [];
 
-var infoWindow;
-var user_id;
+	var infoWindow;
+	var user_id;
 
-var filter = [];
-filter['project'] = "-1";
-filter['genus'] = "-1";
-filter['ageMin'] = "Quaternary";
-filter['ageMax'] = 'Precambrian';
-filter['collector'] = '-1';
+	var filter = [];
+	filter['project'] = "-1";
+	filter['genus'] = "-1";
+	filter['ageMin'] = "Quaternary";
+	filter['ageMax'] = 'Precambrian';
+	filter['collector'] = '-1';
 
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
-
-function clearMarkers() {
-  setMapOnAll(null);
-}
-
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
-}
-
-var getPin = function(color){
-	return "http://www.googlemapsmarkers.com/v1/"+color+"/"
-}
-
-function deselect_all_marker(){
-	for (var i=0; i < selected_markers.length; i++){
-		selected_markers[i].setIcon(getPin("009900"));
-	}
-}
-
-function select_marker(marker){
-	selected_markers.push(marker);
-	marker.setIcon(getPin("DD0000"));
-}
-
-function deselect_marker(marker, index){
-	selected_markers.splice(index,1);
-	marker.setIcon(getPin("009900"));
-}
-
-function createMarkers(info, http){
-
-	var marker = new google.maps.Marker({
-		map: actualmap,
-		position: new google.maps.LatLng(info['lat'], info['lng']),
-		title: info['title'],
-		/*icon: {
-	        path: google.maps.SymbolPath.CIRCLE,
-	        scale: 6,
-	        fillColor: "#F00",
-	        fillOpacity: 1,
-	        strokeWeight: 0.4
-	    }, */
-	    //icon: pinSymbol('#fff'),
-	    icon: getPin("009900"),
-	    text:"false"
-	});
-	
-	marker.addListener("mouseover", function(){
-		infoWindow.close;
-		infoWindow.setContent(info["content"]);
-		logActivity(http, "Hover on fossil "+info['id']+" "+info['title'], user_id);
-		document.getElementById('modal-image-title').innerHTML = "Fossil details";
-		document.getElementById('modal-image-body').innerHTML =
-		"<div class='row'>"
-			+ "<div class='col-md-12'>"
-				+ "<img class='img-responsive' src= "+info["url"]+"></img>"
-			+ "</div>"
-			+ "<div class='col-md-4'>"
-				+ "<p><strong>Genus : </strong>" + info['title'] + "</p>"
-			+ "</div>"
-			+ "<div class='col-md-4'>"
-				+ "<p><strong>Species : </strong>" + info['species'] + "</p>"
-			+ "</div>"
-			+ "<div class='col-md-4'>"
-				+ "<p><strong>Age : </strong>" + info['age'] + "</p>"
-			+ "</div>"
-			+ "<div class='col-md-8'>"
-				+ "<p><strong>Location : </strong>" + info['location'] + "</p>"
-			+ "</div>"
-			+ "<div class='col-md-4'>"
-				+ "<p><strong>Collector : </strong>" + info['collector'] + "</p>"
-			+ "</div>"
-		+ "</div>";
-
-		infoWindow.open('actualmap', marker);
-	});
-	
-	marker.addListener("click", function(){
-
-		var index = selected_markers.indexOf(marker);
-		if (index==-1){
-			select_marker(marker);
-			logActivity(http, "Fossil selected "+info['id']+" "+info['title'], user_id)
-		} else {
-			deselect_marker(marker, index);
-			logActivity(http, "Fossil deselected "+info['id']+" "+info['title'], user_id)
-
+	// Sets the map on all markers in the array.
+	function setMapOnAll(map) {
+		for (var i = 0; i < markers.length; i++) {
+			markers[i].setMap(map);
 		}
-		console.log(index);
-		console.log(selected_markers.length);
-	});
+	}
 
-	markers.push(marker);
-}
+	function clearMarkers() {
+	 	setpOnAll(null);
+	}
 
-function refresh(http)
-{
-	deleteMarkers();
+	function deleteMarkers() {
+		clearMarkers();
+		markers = [];
+	}
 
-	infoWindow = new google.maps.InfoWindow({maxWidth:400});
+	var getPin = function(color){
+		return "http://www.googlemapsmarkers.com/v1/"+color+"/"
+	}
 
-	//retrieve the fossils and put them as marker in the map
-	http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/'+filter['collector']+'/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
-		data.forEach(function(item, index){
-			var info = [];
-			info['lat'] = item['lat'];
-			info['lng'] = item['lng'];
-			info['id'] = item['data_id'];
-			info['location'] = item['place']+ ' ' + item['country'];
-			info['url'] = item['url'];
-			info['age'] = item['age'];
-			info['species'] = item['species'];
-			info['collector'] = item['collector'];
+	function deselect_all_marker(){
+		for (var i=0; i < selected_markers.length; i++){
+			selected_markers[i].setIcon(getPin("009900"));
+		}
+	}
 
-			var info_window_genus = "";
+	function select_marker(marker){
+		selected_markers.push(marker);
+		marker.setIcon(getPin("DD0000"));
+	}
 
-			if (item['genus'] == 'Not listed')
-			{
-				info['title'] = item['genuscustom'] + " " + item['species'];
-				info_window_genus = item['genuscustom'];
+	function deselect_marker(marker, index){
+		selected_markers.splice(index,1);
+		marker.setIcon(getPin("009900"));
+	}
+
+	function createMarkers(info, http){
+
+		var marker = new google.maps.Marker({
+			map: actualmap,
+			position: new google.maps.LatLng(info['lat'], info['lng']),
+			title: info['title'],
+			/*icon: {
+		        path: google.maps.SymbolPath.CIRCLE,
+		        scale: 6,
+		        fillColor: "#F00",
+		        fillOpacity: 1,
+		        strokeWeight: 0.4
+		    }, */
+		    //icon: pinSymbol('#fff'),
+		    icon: getPin("009900"),
+		    text:"false"
+		});
+		
+		marker.addListener("mouseover", function(){
+			infoWindow.close;
+			infoWindow.setContent(info["content"]);
+			logActivity(http, "Hover on fossil "+info['id']+" "+info['title'], user_id);
+			document.getElementById('modal-image-title').innerHTML = "Fossil details";
+			document.getElementById('modal-image-body').innerHTML =
+			"<div class='row'>"
+				+ "<div class='col-md-12'>"
+					+ "<img class='img-responsive' src= "+info["url"]+"></img>"
+				+ "</div>"
+				+ "<div class='col-md-4'>"
+					+ "<p><strong>Genus : </strong>" + info['title'] + "</p>"
+				+ "</div>"
+				+ "<div class='col-md-4'>"
+					+ "<p><strong>Species : </strong>" + info['species'] + "</p>"
+				+ "</div>"
+				+ "<div class='col-md-4'>"
+					+ "<p><strong>Age : </strong>" + info['age'] + "</p>"
+				+ "</div>"
+				+ "<div class='col-md-8'>"
+					+ "<p><strong>Location : </strong>" + info['location'] + "</p>"
+				+ "</div>"
+				+ "<div class='col-md-4'>"
+					+ "<p><strong>Collector : </strong>" + info['collector'] + "</p>"
+				+ "</div>"
+			+ "</div>";
+
+			infoWindow.open('actualmap', marker);
+		});
+		
+		marker.addListener("click", function(){
+
+			var index = selected_markers.indexOf(marker);
+			if (index==-1){
+				select_marker(marker);
+				logActivity(http, "Fossil selected "+info['id']+" "+info['title'], user_id)
+			} else {
+				deselect_marker(marker, index);
+				logActivity(http, "Fossil deselected "+info['id']+" "+info['title'], user_id)
+
 			}
-			else{
-				info['title'] = item['genus'] + " " + item['species'];
-				info_window_genus = item['genus'];
-			}
-			
-			info['content'] = 
-			"<div class='container-fluid map-infowindow'>"
-				+ "<div class='row'>"
+			console.log(index);
+			console.log(selected_markers.length);
+		});
 
-					+ "<div class='col-md-6'>"
-						+ "<img data-toggle='modal' data-target='#Modal-lg-image' src='"+item["url"]+"' class='map-infowindow-img' onclick='show_img(\""+item['url']+"\")'>"
-					+ "</div>"
+		markers.push(marker);
+	}
 
-					+ "<div class='col-md-6'>"
+	function refresh(http)
+	{
+		deleteMarkers();
 
-						+ "<div class='row'>"
-							+ "<div class='col-xs-12'>"
-								+ "<p class='infowindow-text'><strong> Genus : </strong> " + info_window_genus + "</p>"
-							+ "</div>"
-							+ "<div class='col-xs-12'>"
-								+ "<p class='infowindow-text'> <strong> Species : </strong> " + item["species"] + "</p>"
-							+ "</div>"
-							+ "<div class='col-xs-12'>"
-								+ "<p class='infowindow-text'> <strong> Age : </strong>" + item['age'] + "</p>"
-							+ "</div>"
-							+ "<div class='col-xs-12'>"
-								+ "<p class='infowindow-text'> <strong> Collector : </strong>"+ item["collector"] + "</p>"
-							+ "</div>"
-							+ "<div class='col-xs-12'>"
-								+ "<p class='infowindow-text'> <strong> Location : </strong>" + item["place"] + " " + item["country"] + "</p>"
-							+ "</div>"
-							+ "<div class='col-xs-12'>"
-								+ "<a class='infowindow-text' href='#'>Wrong spot ?</a>"
-							+ "</div>"
-							+ "<div class='col-xs-12'>"
-								+ "<button type='button' class='btn btn-primary'>Select this fossil</button>"
-							+ "</div>"
-						+"</div>"
+		infoWindow = new google.maps.InfoWindow({maxWidth:400});
 
+		//retrieve the fossils and put them as marker in the map
+		http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/'+filter['collector']+'/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
+			data.forEach(function(item, index){
+				var info = [];
+				info['lat'] = item['lat'];
+				info['lng'] = item['lng'];
+				info['id'] = item['data_id'];
+				info['location'] = item['place']+ ' ' + item['country'];
+				info['url'] = item['url'];
+				info['age'] = item['age'];
+				info['species'] = item['species'];
+				info['collector'] = item['collector'];
+
+				var info_window_genus = "";
+
+				if (item['genus'] == 'Not listed')
+				{
+					info['title'] = item['genuscustom'] + " " + item['species'];
+					info_window_genus = item['genuscustom'];
+				}
+				else{
+					info['title'] = item['genus'] + " " + item['species'];
+					info_window_genus = item['genus'];
+				}
+				
+				info['content'] = 
+				"<div class='container-fluid map-infowindow'>"
+					+ "<div class='row'>"
+
+						+ "<div class='col-md-6'>"
+							+ "<img data-toggle='modal' data-target='#Modal-lg-image' src='"+item["url"]+"' class='map-infowindow-img' onclick='show_img(\""+item['url']+"\")'>"
+						+ "</div>"
+
+						+ "<div class='col-md-6'>"
+
+							+ "<div class='row'>"
+								+ "<div class='col-xs-12'>"
+									+ "<p class='infowindow-text'><strong> Genus : </strong> " + info_window_genus + "</p>"
+								+ "</div>"
+								+ "<div class='col-xs-12'>"
+									+ "<p class='infowindow-text'> <strong> Species : </strong> " + item["species"] + "</p>"
+								+ "</div>"
+								+ "<div class='col-xs-12'>"
+									+ "<p class='infowindow-text'> <strong> Age : </strong>" + item['age'] + "</p>"
+								+ "</div>"
+								+ "<div class='col-xs-12'>"
+									+ "<p class='infowindow-text'> <strong> Collector : </strong>"+ item["collector"] + "</p>"
+								+ "</div>"
+								+ "<div class='col-xs-12'>"
+									+ "<p class='infowindow-text'> <strong> Location : </strong>" + item["place"] + " " + item["country"] + "</p>"
+								+ "</div>"
+								+ "<div class='col-xs-12'>"
+									+ "<a class='infowindow-text' href='#'>Wrong spot ?</a>"
+								+ "</div>"
+								+ "<div class='col-xs-12'>"
+									+ "<button type='button' class='btn btn-primary'>Select this fossil</button>"
+								+ "</div>"
+							+"</div>"
+
+						+ "</div>"
 					+ "</div>"
 				+ "</div>"
-			+ "</div>"
-			;
+				;
 
-			createMarkers(info, http);	
+				createMarkers(info, http);	
+			});
 		});
-	});
-}
+	}
 
-function refreshFeedback(http){
+	function refreshFeedback(http){
 
-	var feedbacks = "";
-	http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/'+filter['collector']+'/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
-		data.forEach(function(item, index){
-			//feedbacks += 
+		var feedbacks = "";
+		http.get('/api/map/loadfossils/'+filter['genus']+'/-1/ee/ee/'+filter['collector']+'/-1/-1/-1/-1/-1').success(function(data, status, headers, config){
+			data.forEach(function(item, index){
+				//feedbacks += 
+			});
 		});
-	});
-}
+	}
 
 
-function logActivity(http, message, user_id){
-	activity = {};
-	activity.activity = message;
-	activity.user_id = user_id;
-	// Do the ajax call
-	http({
-        method : 'POST',
-        url: '/api/map/logmapactivity',
-        data: $.param(activity),
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    	
-	}).success(function(data, status, headers, config) {
-		console.log("success");
-	});
-}
+	function logActivity(http, message, user_id){
+		activity = {};
+		activity.activity = message;
+		activity.user_id = user_id;
+		// Do the ajax call
+		http({
+	        method : 'POST',
+	        url: '/api/map/logmapactivity',
+	        data: $.param(activity),
+	        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	    	
+		}).success(function(data, status, headers, config) {
+			console.log("success");
+		});
+	}
 
-function show_img(url){
-	console.log(url);
-}
+	function show_img(url){
+		console.log(url);
+	}
 
 
-	$scope.loading = false;
 
+	// Map section
 	$scope.map_zoom = 3;
 
 	var mapProp = {
@@ -240,7 +240,7 @@ function show_img(url){
 	};
 
 	actualmap = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	
+
 	actualmap.addListener("click", function(){
 		console.log('map clicked');
 		logActivity($http, "Map Click", user_id);
@@ -266,10 +266,10 @@ function show_img(url){
 		$scope.map_zoom=new_zoom;
 	});
 
-
 	refresh($http);
 
 
+	//Nav bar
 	$scope.profile = {};
 	$scope.profile.first_name = 'stranger, do you fancy a login ?';
 
@@ -285,6 +285,7 @@ function show_img(url){
 
 	});
 
+	//Filter Section
 	$scope.selectedProject = filter['project'];
 	$scope.selectedGenus = filter['genus'];
 	$scope.selectedAgeMin = filter['ageMin'];
@@ -327,7 +328,7 @@ function show_img(url){
 		logActivity($http, $a, user_id);
 	};
 
-	
+
 });
 
 
