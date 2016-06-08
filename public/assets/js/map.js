d0180beb25dc13e0dc949de769b8d1f1a128a94d
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-08 12:29:33 +0100

    tets

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -84,8 +84,11 @@ function createMarkers(info, http){
 	marker.addListener("click", function(){
 		if (selected_markers.indexOf(marker)==-1){
 			selected_markers.push(marker);
-			marker.setIcon()
-		};
+			marker.setIcon(getPin("990000"));
+		} else{
+			selected_markers = selected_markers.splice(selected_markers.indexOf(marker),1);
+			marker.setIcon(getPin("009900"));
+		}
 	});
 
 	markers.push(marker);
