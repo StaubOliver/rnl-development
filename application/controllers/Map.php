c38173ea3b13b797bb7db075fee635b4d1fc075e
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-09 23:49:47 +0100

    admin section

--- application/controllers/Map.php
+++ application/controllers/Map.php
@@ -48,6 +48,34 @@ class map extends CI_Controller {
         	//$this->LoggerModel->logData();
 		//}
 	}
+
+	public function map_admin(){
+		$genus = '-1';
+		$species = '-1';
+		$age_min = '-1';
+		$age_max = '-1';
+		$collector = '-1';
+		$map_lat_ne = '-1';
+		$map_lng_ne = '-1';
+		$map_lat_sw = '-1';
+		$map_lng_sw = '-1';
+		$map_zoom = '-1';
+
+		$filter = array(
+			'genus' => '-1',
+			'species' => '-1',
+			'age_min' => '-1'
+			'age_max' => '-1',
+			'collector' => '-1',
+			'map_lat_ne' => '-1',
+			'map_lng_ne' => '-1',
+			'map_lat_sw' => '-1',
+			'map_lng_sw' => '-1',
+			'map_zoom' => '-1'
+		);
+
+		$data['feedbacks'] = $this->MapModel->loadFeedbacks($filter);
+	}
 /*
 	public function old() {		
 		// Check if logged in
---
+++ application/views/map_admin.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Metadata -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mapping the Fossil' Collection</title>
+	
+	<!-- Typography -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700,100' rel='stylesheet' type='text/css'>
+	
+	<!-- JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+	<script src="/assets/js/map.js"></script>
+	<!-- Icons -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
+	<!-- CSS -->
+	<!--
+	<link rel="stylesheet" href="/assets/css/normalize.css">
+	<link rel="stylesheet" href="/assets/css/skeleton.css">
+	<link rel="stylesheet" href="/assets/css/skeleton-override.css">
+	<link rel="stylesheet" href="/assets/css/style.css">
+	<link rel="stylesheet" href="/assets/css/responsive.css">
+	-->
+	<link rel="stylesheet" href="/assets/css/map2.css">
+	
+	<!-- Google Map API -->
+	<script src="http://maps.googleapis.com/maps/api/js"></script>
+	
+	<!--<script>
+		function initialize() {
+		  var mapProp = {
+		    center:new google.maps.LatLng(51.508742,-0.120850),
+		    zoom:5,
+		    mapTypeId:google.maps.MapTypeId.ROADMAP,
+		    mapTypeControl:false,
+		    streetViewControl:false
+		  };
+		  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+	</script>-->
+
+</head>
+
+<body>
+
+	<nav class="navbar-default  navbar-fixed-top" >
+		<div class="container-fluid">
+
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Map Admin Section</a>
+			</div>
+
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><p class="navbar-text">Hello {{ profile.first_name }}</p></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<?php if($is_admin == 1): ?>
+		 			<li><a href="/map/admin">Back to the map</a></li>
+					<?php endif; ?>	
+					<li><p class="navbar-text">Share</p></li>
+					<?php if($logged_in == true): ?>
+		 			<li><a href ="/auth/logout" class="navbar-link">Logout</a></li>
+					<?php endif; ?>	
+					<?php if($logged_in == false): ?>
+		 			<li><a href ="/auth/login" class="navbar-link">Login</a></li>
+					<?php endif; ?>	
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+
+	<div class='container-fluid'>
+		<div class='row'>
+
+			<?php foreach($feedbacks as $feedback): ?>
+				</div class='col-md-4'>
+
+					<?php echo $feedback->message; ?>
+
+				</div>
+			<?php endforeach; ?>
+
+
+		</div>
+	</div>
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
