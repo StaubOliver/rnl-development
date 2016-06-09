088196e0358907c57291a66b7413a10d613673c3
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-09 13:06:05 +0100

    t

--- application/models/MapModel.php
+++ application/models/MapModel.php
@@ -351,7 +351,7 @@ class MapModel extends CI_Model {
 
         if($this->db->insert('feedback', $data))
         {
-            $query_feedback = $this->db->query("SELECT feedback_id FROM feedback WHERE user_id='"+$data['user_id'] +"' and time='" + $data['time']+"' and message='" + $data['message']+ "' and filter_id='" + $data['filter_id']"' and map_coordinates_id='" + $data['map_coordinates_id']+";'");
+            $query_feedback = $this->db->query("SELECT feedback_id FROM feedback WHERE user_id='"+$data['user_id'] +"' and time='" + $data['time']+"' and message='" + $data['message']+ "' and filter_id='" + $data['filter_id']+"' and map_coordinates_id='" + $data['map_coordinates_id']+";'");
 
             if ($query_feedback->num_rows() > 0)
             {
