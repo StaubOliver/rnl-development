486a2925c551e3feb93794484b45169faadb8070
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-10 11:47:01 +0100

    up votes

--- application/views/map2.php
+++ application/views/map2.php
@@ -373,8 +373,11 @@
 										<div class="col-md-6 feedback-message-time">
 											<p>{{feedback['time']}}</p>
 										</div>
-										<div class="col-md-6 feedback-message-upvote" >
-											<p ng-show="feedback.upvote > 0">{{feedback.upvote}} upvotes</p><button class='btn btn-custom-default btn-xs'>Upvote</button>
+										<div class='col-md-3'>
+											<button class='btn btn-custom-default btn-xs'>Upvote</button>
+										</div>
+										<div class="col-md-3 feedback-message-upvote" >
+											<p ng-show="feedback.upvote > 0">{{feedback.upvote}} upvotes</p>
 										</div>
 										<div class="col-md-6 feedback-message-upvote" >
 											<p ng-show="feedback.selection.length">{{feedback.selection.length}} fossils selected</p>
