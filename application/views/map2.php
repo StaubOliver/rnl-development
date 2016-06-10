e1859d9ba3250fbdd59f42bd6cc7f9e0cb6bbd9b
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-10 12:15:29 +0100

    test

--- application/views/map2.php
+++ application/views/map2.php
@@ -380,7 +380,7 @@
 											<button class='btn btn-custom-default btn-xs'>Upvote</button>
 										</div>
 
-										<div feedback-message-upvote" ng-show="feedback.upvote > 0" ng-class="{ col-md-4:feedback.user_has_upvote, col-md-6 : !feedback.user_has_upvote}">
+										<div class="feedback-message-upvote" ng-show="feedback.upvote > 0" ng-class="{ col-md-4:feedback.user_has_upvote, col-md-6 : !feedback.user_has_upvote}">
 											<p >{{feedback.upvote}} upvotes</p>
 										</div>
 
