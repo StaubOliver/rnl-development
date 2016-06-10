835b7be6a7c79850a88bdd0f6a35b74fe8d0a6ab
StaubOliver <staub.olivier@gmail.com>
2016-06-10 09:14:50 +0100

    css

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -79,7 +79,7 @@
 		<div class='container-fluid'>
 			<div class='row'>
 
-				<div class='col-md-8 col-md-offset-2'>
+				<div class='col-md-8 col-md-offset-2 admin-title'>
 					<h4><?php echo count($feedbacks).' feedbacks recorded'; ?></h4>
 				</div>
 
