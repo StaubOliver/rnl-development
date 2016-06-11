e858d946cf6e1e89941bf153833160437fa85481
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-11 12:52:31 +0100

    delete ben

--- application/views/map2.php
+++ application/views/map2.php
@@ -365,7 +365,12 @@
 						</div>
 
 							<div ng-repeat='feedback in feedbacks' ng-show='feedbacks.length'>
+
+
 								<div class="feedback-message " ng-mouseover="recordActivity('Feedback mouse over '+feedback['feedback_id']+ ' '+feedback['message']); mouseoverFeedback(feedback)" ng-mouseleave="mouseleaveFeedback()">
+									<div class='feedback-message-delete-btn' ng-click=""> 
+										<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+									</div>
 									<div class="row">
 										<div class="col-md-12 feedback-message-text">
 											<p>{{feedback['message']}}</p>
