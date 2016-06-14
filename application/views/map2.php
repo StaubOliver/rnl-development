5ef1b80b0e2547d1c7fa56d54e9383ea64bc2681
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-14 19:19:25 +0100

    Social media

--- application/views/map2.php
+++ application/views/map2.php
@@ -402,7 +402,7 @@
 					<div class="feedback-section">
 
 						<div class="row">
-							<div class="col-md-7 feedback-title">
+							<div class="col-md-12 feedback-title">
 								<h4>Share on your networks</h4><img class='social-network-icon' src="/assets/img/socialnetworks/twitter.png" alt="There should be an awsome logo" height="24" width="24"><img class='social-network-icon' src="/assets/img/socialnetworks/facebook.png" alt="There should be an awsome logo" height="24" width="24"><img class='social-network-icon' src="/assets/img/socialnetworks/google.png" alt="There should be an awsome logo" height="24" width="24">
 							</div>
 						</div>
