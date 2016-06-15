f535f96bcc03843b9c05421974c47d9fbaaf04ec
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-15 17:28:50 +0100

    Rating

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -40,7 +40,7 @@
 
 		var rating_highlight = function(id){
 			console.log(id);
-			if (id=='1'){
+			if (id==1){
 				document.getElementById('rating-1').src="/assets/img/star/star_highlight.png";
 			}
 		}
