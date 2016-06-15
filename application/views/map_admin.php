6747837b894f48c8c17b031945aaa1e358a9a518
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-15 14:01:31 +0100

    Star

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -165,7 +165,7 @@
 									<div class='btn btn-custom-default btn-sm'>See comment on the map</div><div class='btn btn-custom-primary btn-sm'>Delete comment</div>	
 								</div>
 								<div class='col-md-6'>
-									<img src='/assets/img/star/star_empty'><img src='/assets/img/star/star_empty'><img src='/assets/img/star/star_empty'><img src='/assets/img/star/star_empty'><img src='/assets/img/star/star_empty'>
+									<img src='/assets/img/star/star_empty.png'><img src='/assets/img/star/star_empty.png'><img src='/assets/img/star/star_empty.png'><img src='/assets/img/star/star_empty.png'><img src='/assets/img/star/star_empty.png'>
 								</div>
 
 							</div>
