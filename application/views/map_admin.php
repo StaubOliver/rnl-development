7bbb224b8585500754938be4ab044de12b270346
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-15 18:02:37 +0100

    Javascript

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -38,67 +38,67 @@
 	
 	<script>
 
-		var rating_highlight = function(feedback_id, star){
-			console.log(feedback_id+'-'+star);
-			if (star=="1"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
-			}
-			if (star=="2"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
-			}
-			if (star=="3"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_highlight.png";
-			}
-			if (star=="4"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_highlight.png";
-			}
-			if (star=="5"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_highlight.png";
-				document.getElementById('rating-'+feedback_id+'-5').src="/assets/img/star/star_highlight.png";
-			}
-		}
-
-		var rating_unhighlight = function(feedback_id, star){
-			if (star=="1"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
-			}
-			if (star=="2"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
-			}
-			if (star=="3"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_empty.png";
-			}
-			if (star=="4"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_empty.png";
-			}
-			if (star=="5"){
-				document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_empty.png";
-				document.getElementById('rating-'+feedback_id+'-5').src="/assets/img/star/star_empty.png";
-			}
-		}
+		
 
 
 		var map = angular.module('map_admin', []).controller('admin_map_feedbacks', function($scope, $http, $compile){
 
+			var rating_highlight = function(feedback_id, star){
+				console.log(feedback_id+'-'+star);
+				if (star=="1"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
+				}
+				if (star=="2"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
+				}
+				if (star=="3"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_highlight.png";
+				}
+				if (star=="4"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_highlight.png";
+				}
+				if (star=="5"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_highlight.png";
+					document.getElementById('rating-'+feedback_id+'-5').src="/assets/img/star/star_highlight.png";
+				}
+			}
 
+			var rating_unhighlight = function(feedback_id, star){
+				if (star=="1"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
+				}
+				if (star=="2"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
+				}
+				if (star=="3"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_empty.png";
+				}
+				if (star=="4"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_empty.png";
+				}
+				if (star=="5"){
+					document.getElementById('rating-'+feedback_id+'-1').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-2').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-3').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-4').src="/assets/img/star/star_empty.png";
+					document.getElementById('rating-'+feedback_id+'-5').src="/assets/img/star/star_empty.png";
+				}
+			}
 
 		});
 	
