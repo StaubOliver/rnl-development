5a501e0144563d00571a99bc3dc37fa628c1adfb
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-16 22:55:33 +0100

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -336,8 +336,6 @@
 						<div class="row filter-element">
 							<div class="col-md-12">
 								<p class='filter-element-title'>Genus</p>
-							</div>
-							<div class="col-md-12">
 								<select class="form-control" id="fossilGenusSelect" ng-model="selectedGenus" ng-change="clear_selected_markers(); newGenus()" ng-mouseover="recordActivity('Genus Selector Hover')">
 									<option value="-1" selected>All Genera</option>
 									<optgroup label="Coral genera">	
