2ac4d12f1f7f4172e30262e78e101340ba84b0ab
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-16 11:04:52 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -462,7 +462,7 @@ var map = angular.module('map', ['rzModule'])
 			//refreshFeedback($http);
 			for(var i = 0; i < $scope.feedbacks.length; i++)
 			{
-				if ($scope.feedbacks[i] == id){
+				if ($scope.feedbacks[i] == parseInt(id)){
 					console.log(('update'));
 					$scope.feedbacks[i]['upvote'] += 1;
 					$scope.feedbacks[i]['user_has_upvote'] = true;
