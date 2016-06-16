7ef287c9b03f265240af86ea1a3957d9ddd844b7
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-16 17:38:51 +0100

    Admin

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -241,7 +241,7 @@
 					<!-- replies -->
 					<div ng-repeat='rep in feedback.replies' ng-show='feedback.replies.length'>
 
-						<div class='col-md-6 col-md-offset-3'>
+						<div class='col-md-8 col-md-offset-2'>
 							<div class='admin-feedback-reply'>
 
 								<div class='row'>
