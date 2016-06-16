c5f647ee862eac06f4bfc7dc00ddc5bfdf125447
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-06-16 16:44:26 +0100

    Up vote

--- application/views/map2.php
+++ application/views/map2.php
@@ -499,8 +499,8 @@
 										</div>-->
 
 										<div class="col-md-6 feedback-message-buttons">
-											<a class='feedback-message-button-up' href='#' ng-show="!feedback['user_has_upvote']" ng-click="upvoteFeedback(feedback.feedback_id)">Upvote</a> 
-											<span ng-show="{{feedback.user_has_upvote}}">Upvoted</span>
+											<a class='feedback-message-button-up' href='#' ng-show="!feedback.user_has_upvote" ng-click="upvoteFeedback(feedback.feedback_id)">Upvote</a> 
+											<span ng-show="feedback.user_has_upvote">Upvoted</span>
 											<a class='feedback-message-button-reply' href='#write-section' ng-click="replyFeedback(feedback.feedback_id)">Reply</a>
 										</div>
 
@@ -544,8 +544,8 @@
 											</div>
 
 											<div class="col-md-6 feedback-message-buttons">
-											<a ng-show="{{!rep.user_has_upvote}}" class='feedback-message-button-up' href='#' ng-click="upvoteFeedback(rep.feedback_id)">Upvote</a>
-											<span ng-show="{{rep.user_has_upvote}}">Upvoted</span>
+											<a ng-show="!rep.user_has_upvote" class='feedback-message-button-up' href='#' ng-click="upvoteFeedback(rep.feedback_id)">Upvote</a>
+											<span ng-show="rep.user_has_upvote">Upvoted</span>
 										</div>
 
 										<div class="col-md-6 feedback-message-info">
