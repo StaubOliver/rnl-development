8d71af1264b89fcf3599732f80db783f76007516
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-05 18:33:04 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -796,7 +796,6 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 	
 	function refreshFeedback(){
 		$scope.feedbacks = [];
-		$scope.rating_img = [];
 		$http.get('/api/map/loadAdminFeedback/').success(function(data, status, headers, config){
 			data.forEach(function(item, index)
 			{
