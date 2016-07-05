82eb71da61f1a66d04392353c3bc3d48e037f16c
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-05 23:20:47 +0100

    test

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -189,67 +189,64 @@
 					<!-- replies -->
 					<div ng-repeat='rep in feedback.replies' ng-show='feedback.replies.length'>
 
-						<div class='col-md-7'>
-							<div class='admin-feedback-reply'>
+						<div class='col-md-10 col-md-offset-1 admin-feedback-reply'>
 
-								<div class='row'>
-										
-									<div class='col-md-6'>
-										Feedback from {{rep["first_name"]}} {{rep["last_name"]}}
-									</div>
-
-									<div class='col-md-6'>
-										{{rep["upvote"]}} upvotes
-									</div>
+							<div class='row'>
 									
-									<div class='col-md-12'>
-										<strong>
-											{{rep['message']}}
-										</strong>
-									</div>
-									
-									<div class='col-md-6'>
-										{{rep['time']}}
-									</div>
-									
-									<div class='col-md-6'>
-										{{rep['selection'].length}} fossils selected
-									</div>
-
-									<div class='col-md-6'>
-										From: {{rep['age_min']}}
-									</div>
-
-									<div class='col-md-6'>
-										To: {{rep['age_max']}}
-									</div>
-
-									<div class='col-md-6'>
-										<div ng-show="rep['genus']==-1">
-											Genus: All
-										</div>
-										<div ng-show="rep['genus']!=-1">
-											Genus: {{rep['genus']}}
-										</div>
-									</div>
-
-									<div class='col-md-6'>
-										<div ng-show="rep['genus']==-1">
-											Collector: All
-										</div>
-										<div ng-show="rep['genus']!=-1">
-											Collector: {{rep['collector']}}
-										</div>
-									</div>
-
-									<div class='col-md-12 admin-feedback-tool'>
-										<div class='btn btn-custom-default btn-sm'>See comment on the map</div>
-										<div class='btn btn-custom-primary btn-sm'>Delete comment</div>	
-									</div>
-
+								<div class='col-md-6'>
+									Feedback from {{rep["first_name"]}} {{rep["last_name"]}}
 								</div>
 
+								<div class='col-md-6'>
+									{{rep["upvote"]}} upvotes
+								</div>
+								
+								<div class='col-md-12'>
+									<strong>
+										{{rep['message']}}
+									</strong>
+								</div>
+								
+								<div class='col-md-6'>
+									{{rep['time']}}
+								</div>
+								
+								<div class='col-md-6'>
+									{{rep['selection'].length}} fossils selected
+								</div>
+
+								<div class='col-md-6'>
+									From: {{rep['age_min']}}
+								</div>
+
+								<div class='col-md-6'>
+									To: {{rep['age_max']}}
+								</div>
+
+								<div class='col-md-6'>
+									<div ng-show="rep['genus']==-1">
+										Genus: All
+									</div>
+									<div ng-show="rep['genus']!=-1">
+										Genus: {{rep['genus']}}
+									</div>
+								</div>
+
+								<div class='col-md-6'>
+									<div ng-show="rep['genus']==-1">
+										Collector: All
+									</div>
+									<div ng-show="rep['genus']!=-1">
+										Collector: {{rep['collector']}}
+									</div>
+								</div>
+
+								<div class='col-md-12 admin-feedback-tool'>
+									<div class='btn btn-custom-default btn-sm'>See comment on the map</div>
+									<div class='btn btn-custom-primary btn-sm'>Delete comment</div>	
+								</div>
 							</div>
+
 						</div>
 					</div>
 
