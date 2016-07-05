7dcd7aa52598832f0ac855691ba3341b1d81b563
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-05 18:39:40 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -803,15 +803,15 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 				$scope.rating_img[item["feedback_id"]] = [];
 				
 				//rating correctness
-				if (item['rating_correctness'] == "0")){
+				if (item['rating_correctness'] == "0"){
 					$scope.rating_img[item['feedback_id']][1] = url_empty;
 					$scope.rating_img[item['feedback_id']][2] = url_empty;
 				}
-				if (item['rating_correctness'] == "1")){
+				if (item['rating_correctness'] == "1"){
 					$scope.rating_img[item['feedback_id']][1] = url_hightlight;
 					$scope.rating_img[item['feedback_id']][2] = url_empty;
 				}
-				if (item['rating_correctness'] == "2")){
+				if (item['rating_correctness'] == "2"){
 					console.log('test');
 					$scope.rating_img[item['feedback_id']][1] = url_empty;
 					$scope.rating_img[item['feedback_id']][2] = url_hightlight;
