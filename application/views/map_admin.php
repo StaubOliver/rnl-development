437fafb7f4a49d7d77b66e6a4a77af79d88938f6
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-05 18:29:56 +0100

    Test

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -140,7 +140,7 @@
 							<div class='col-md-5'>
 								<div class='row'>
 									<div class='col-md-12'>
-										Contribution rating
+										Contribution rating  {{rating_img[feedback[96]][1]}}
 									</div>
 									<!--
 									<div class='row'>
@@ -187,7 +187,7 @@
 									<!-- rating incorrect - correct -->
 									<div class="row rating-correctness">
 										<div class="col-md-6 rating-left">
-											<div class='btn btn-custom-default btn-xs rating-btn-left' ng-click="rating_click(feedback['feedback_id'], 1, 1)">Incorrect {{rating_img[feedback['feedback_id']][1]}}"<img id="rating-{{feedback['feedback_id']}}" class='rating-btn-img rating-btn-img-left' ng-src="{{rating_img[feedback['feedback_id']][1]}}"></div>
+											<div class='btn btn-custom-default btn-xs rating-btn-left' ng-click="rating_click(feedback['feedback_id'], 1, 1)">Incorrect<img id="rating-{{feedback['feedback_id']}}" class='rating-btn-img rating-btn-img-left' ng-src="{{rating_img[feedback['feedback_id']][1]}}"></div>
 
 										</div>
 
