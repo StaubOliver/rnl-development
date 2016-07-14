4297b786db57b421cdb6d3288828c4fc344d1145
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-14 14:19:39 +0100

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -99,8 +99,8 @@
 					<?php endif; ?>	
 
 					<li><p class="navbar-text">Share</p></li>
-					<li><img class='social-network-icon' src="/assets/img/socialnetworks/twitter_white.png" alt="There should be an awsome logo" href="http://www.twitter.com/share?text=Check out Manchester Museum's fossil collection ! @McrMuseum @TheStudyMcr&url=https://{{share_url}}/map" target="#" height="32" width="32"></li>
-					<li><img class='social-network-icon' src="/assets/img/socialnetworks/facebook_white.png" alt="There should be an awsome logo" height="32" width="32"></li>
+					<li><a href="http://www.twitter.com/share?text=Check out Manchester Museum's fossil collection ! @McrMuseum @TheStudyMcr&url=https://{{share_url}}/map" target="#"><img class='social-network-icon' src="/assets/img/socialnetworks/twitter_white.png" alt="There should be an awsome logo" height="32" width="32"></a></li>
+					<li><img class='social-network-icon' src="/assets/img/socialnetworks/facebook_white.png" alt="Twitter" height="32" width="32"></li>
 					<li><img class='social-network-icon' src="/assets/img/socialnetworks/google_white.png" alt="There should be an awsome logo" height="32" width="32"></li>
 
 					<?php if($logged_in == true): ?>
