f3e3cad0cb48d050ccfb6bcae6fa0849abf26e18
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-14 15:17:05 +0100

    test

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -147,8 +147,12 @@
 										<div class='btn btn-custom-default btn-xs btn-admin btn-admin-map' ng-click='showMap(feedback.feedback_id, 0)'>See on the map</div>
 									</div>
 
-									<div class='col-md-12'>
+									<div class='col-md-12' ng-show="feedback.hidden == 0">
 										<div class='btn btn-custom-primary btn-xs btn-admin btn-admin-hide'>Hide comment</div>	
+									</div>
+
+									<div class='col-md-12' ng-show="feedback.hidden == 1">
+										<div class='btn btn-custom-primary btn-xs btn-admin btn-admin-hide'>Unhide comment</div>	
 									</div>
 
 								</div>
