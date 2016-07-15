3c5ca7f54a07fe11f20d77021f910b63fe812ae6
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-15 18:15:37 +0100

    T

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1196,8 +1196,10 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 				    icon: getPinColor(feedback.selection[i]['age'])
 				});
 		
-				marker.addListener("click", function(){
+				marker.addListener("click", function()
+				{
 
+					console.log(feedback['selection'][i]);
 					//info window
 					infoWindowadmin.close;
 					var content =
