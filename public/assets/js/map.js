6b864256b55e61cedb06bca347798a4034a3abf7
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-15 14:54:54 +0100

    T

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1239,7 +1239,7 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 				var marker = new google.maps.Marker({
 					map: actualmap,
 					position: new google.maps.LatLng($temp.selection[i]['lat'], $temp.selection[i]['lng']),
-					title: info['id'] + "-" +$temp.selection[i]['title'],
+					title: $temp.selection[i]['id'] + "-" +$temp.selection[i]['title'],
 				    icon: getPinColor($temp.selection[i]['age'])
 				});
 		
