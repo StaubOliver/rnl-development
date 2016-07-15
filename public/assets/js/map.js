33c126e31821f9c829e310ac76c9d620e035ea34
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-15 18:11:07 +0100

    T

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1187,7 +1187,7 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 		if (feedback.selection.length > 0){
 			for (var i = 0; i < feedback.selection.length; i++)
 			{
-				console.log(feedback.selection[i]);
+				//console.log(feedback.selection[i]);
 
 				var marker = new google.maps.Marker({
 					map: actualmap,
