e20d7d325214a70163f38e5a9745b4168de96caa
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-15 15:38:33 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1224,7 +1224,7 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 		};
 
 		actualmap = new google.maps.Map(document.getElementById("map-"+feedback_id),mapOpt);
-		actualmap.fitBounds(google.maps.LatLngBounds(LatLng($temp.selection[i]['map_sw_lat'], $temp.selection[i]['map_sw_lng']), LatLng($temp.selection[i]['map_ne_lat'], $temp.selection[i]['map_ne_lng'])))
+		actualmap.fitBounds(google.maps.LatLngBounds(google.maps.LatLng($temp.selection[i]['map_sw_lat'], $temp.selection[i]['map_sw_lng']), google.maps.LatLng($temp.selection[i]['map_ne_lat'], $temp.selection[i]['map_ne_lng'])))
 
 		if ($temp.selection.length > 0){
 			for (var i = 0; i < $temp.selection.length; i++)
