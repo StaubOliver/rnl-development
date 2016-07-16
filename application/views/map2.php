c1863c37190a62bf78067cc6d4b84a8d6ba01361
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-16 01:18:25 +0100

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -390,8 +390,15 @@
 
 									<div class="row">
 
-										<div class="col-md-12 feedback-message-author">
+										<div class="col-md-9 feedback-message-author">
 											<p>{{feedback['first_name']}} {{feedback['last_name']}} on {{feedback['time'].split(" ")[0]}}</p>
+										</div>
+
+										<div class='col-md-3'>
+											<img class='social-network-icon' src="/assets/img/socialnetworks/twitter_light.png" alt="Twitter" height="18" width="18">
+											<img class='social-network-icon' src="/assets/img/socialnetworks/facebook_light.png" alt="Twitter" height="18" width="18">
+											<img class='social-network-icon' src="/assets/img/socialnetworks/google_light.png" alt="Twitter" height="18" width="18">
+
 										</div>
 
 										<div class="col-md-12 feedback-message-text">
