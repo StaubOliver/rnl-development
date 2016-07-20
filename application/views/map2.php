dc5c521c99f5e2c2d761db4be1e51f6d2a70b362
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-20 16:39:46 +0200

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -156,13 +156,14 @@
 	<div class="modal fade" id="ModalHelp" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
+			
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="modal-image-title"></h4>
+					<h4 class="modal-title" id=""></h4>
 				</div>
-				<div class="modal-body" id="modal-image-body">
-	
 
+				<div class="modal-body" id="">
+					<p>Test</p>	
 				</div>
 			</div>
 		</div>
