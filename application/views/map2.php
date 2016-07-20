b98d8704f37e7a59ca3d54d52ca538807d6a00cb
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-07-20 16:16:46 +0200

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -133,7 +133,7 @@
 					</li>
 
 					<li>
-						<a href="" data-toggle='modal-help' data-target='#Modal-help'>
+						<a href="" data-toggle='modal' data-target='#Modal-help'>
 							Help
 						</a>
 					</li>
@@ -152,20 +152,20 @@
 		</div>
 	</nav>
 
-	<div class="modal fade modal-help" id="Modal-help" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-					<div class="modal-dialog modal-lg" role="document">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-								<h4 class="modal-title" id="modal-image-title"></h4>
-							</div>
-							<div class="modal-body" id="modal-image-body">
-				
-
-							</div>
-						</div>
-					</div>
+	<div class="modal fade" id="Modal-help" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="modal-image-title"></h4>
 				</div>
+				<div class="modal-body" id="modal-image-body">
+	
+
+				</div>
+			</div>
+		</div>
+	</div>
 
 
 	<div class="map-header">
