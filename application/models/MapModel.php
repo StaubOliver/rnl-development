71c2b0956002424888ca0243b4ac7cddd1b21d7c
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-01 21:08:01 +0100

    Test

--- application/models/MapModel.php
+++ application/models/MapModel.php
@@ -734,7 +734,28 @@ class MapModel extends CI_Model {
     }*/
 
 
-   
+    public function updatelocationestimate()
+    {
+        $res = 0;
+
+        $query_projects = $this->db->query('SELECT id, name, image, blurb, data_table, image_table FROM projects_master');
+
+        if ($query_projects->num_rows() > 0)
+        {
+            foreach($query_fossil->result_array() as $row)
+            {
+                $query_fossils = $this->db->query('SELECT * FROM '.$row['data_table'].' WHERE ((country!="Missing" and place!="") or (country!="" and place!="")) and (lat=0 or lat IS NULL) and (lng=0 or lng IS NULL)');
+
+                $res += $query_fossils->num_rows();
+            }
+        }
+        else 
+        {
+            $res = 0;
+        }
+
+        return $res;
+    }
 
     function updatelocation($data)
     {
@@ -768,7 +789,7 @@ class MapModel extends CI_Model {
 
                         $query_already_exist = $this->db->query('SELECT data_id, lat, lng  FROM ' . $row["data_table"].' WHERE lat='.$temp[0].' AND lng='.$temp[1].' limit 1300, 800');
 
-                        if ($query_already_exist->num_rows() = 0)
+                        if ($query_already_exist->num_rows() == 0)
                         {
                             $this->db->where('data_id',$row['data_id']);
 
