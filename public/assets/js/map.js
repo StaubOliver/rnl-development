d8447bff17e0573d47a171fa2bb2573c1b8976ff
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-02 12:42:53 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1025,6 +1025,7 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 		}).success(function(data, status, headers, config) {
 
 		}).error(function(data, status, headers, config){
+			$scope.showUpdateError = true;
 			document.getElementById('updateLocationError').innerHTML='An error occured'+data;
 			console.log(data);
 		});
