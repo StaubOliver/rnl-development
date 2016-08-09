54e2b8473383a4eccb53b733641ddcd5efabd5fb
StaubOliver <staub.olivier@gmail.com>
2016-08-09 20:03:25 +0100

    test

--- application/models/MapModel.php
+++ application/models/MapModel.php
@@ -996,7 +996,7 @@ class MapModel extends CI_Model {
 
         $query_unique_id = $this->db->query("select distinct unique_id from map_activity where unique_id!='12f3bdd3b95558e788f1a602a1412e3d07e5f74a' and unique_id!='1618315f0f87047126d4d684950537ef2ce69bd5' and unique_id!='25a0288f2636eefb53dc1b4ad28b7da44f91ca90' and unique_id!='5504539e6c4db715a72a5a6b8875be5e5f443390' and unique_id!='898850774d78fdf45cacf3239c132a76a7bcd572' and unique_id!='db57dc7ed8fac52c3688c3f74f96be93386408f1';");
         $nb_visitors = $query_unique_id->num_rows();
-        $avg_time;
+        $avg_time = 0;
         foreach ($query_unique_id->result_array() as $unique) 
         {
             $query_visit_start = $this->db->query("SELECT activity_id, time from map_activity where unique_id='".$unique["unique_id"]."' order by activity_id asc limit 1");
