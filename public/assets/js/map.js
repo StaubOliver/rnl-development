01306704ebf9fd3c48f8602adc670121bbf0e9c5
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-10 16:06:18 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1488,7 +1488,7 @@ var map_stats = angular.module('map_stats', []).controller('admin_map_stats', fu
 			    var data = google.visualization.arrayToDataTable($scope.general.hist_actions);
 
 			    var options = {
-			      title: 'Lengths of dinosaurs, in meters',
+			      title: 'Actions',
 			      legend: { position: 'none' }, 
 			      colors: ['212a33']
 
