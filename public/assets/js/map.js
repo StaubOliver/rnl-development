6d2f942fe617c871a089bd0f1e2f9fc734a028e5
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-10 11:52:58 +0100

    Test

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -888,7 +888,7 @@ var map = angular.module('map', ['rzModule'])
 		refreshFeedback($http);
 		refresh($http);
 	}
-
+/*
 
 	window.onbeforeunload = function () {
     	$scope.recordActivity("Close Page", "");
@@ -896,7 +896,7 @@ var map = angular.module('map', ['rzModule'])
 
 	window.onunload = function(){
 		$scope.recordActivity("Close Page", "");
-	}
+	}*/
 /*
 	window.onbeforeunload = bunload;
 
@@ -914,6 +914,10 @@ var map = angular.module('map', ['rzModule'])
 		   return false; 
 	};
 */
+
+	$( window ).unload(function() {
+		$scope.recordActivity("Close Page", "");
+	});
 
 });
 
