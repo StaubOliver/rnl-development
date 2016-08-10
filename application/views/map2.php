10398f5316603436b795059a91b373571ec7c2f7
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-10 19:56:03 +0100

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -158,7 +158,7 @@
 			<div class="modal-content">
 
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<button type="button" class="btn btn-custom-default" data-dismiss="modal" aria-label="Close">Close<span aria-hidden="true">&times;</span></button>
 					<h4 class="modal-title">Mapping Manchester Museum Fossil Collection</h4>
 				</div>
 
