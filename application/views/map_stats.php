57c0a47b432d478fd41bd0cafe6bf13a0a7fb7f4
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-10 21:39:05 +0100

    Test

--- application/views/map_stats.php
+++ application/views/map_stats.php
@@ -513,7 +513,7 @@
 							<button style='margin-top:8px' type="button" class="btn btn-xs btn-custom-default" ng-click="show_dwell_fct_actions_data = !show_dwell_fct_actions_data">Show Data</button>
 						</div>
 						<div style="display:block; text-align:right" class='col-md-6' ng-show="show_dwell_fct_actions_data">
-							<button style='margin-top:8px' type="button" class="btn btn-xs btn-custom-default" ng-click="show_dwell_fct_actions_data = !show_dwell_fct_actions_data">Show Data</button>
+							<button style='margin-top:8px' type="button" class="btn btn-xs btn-custom-default" ng-click="show_dwell_fct_actions_data = !show_dwell_fct_actions_data">Hide Data</button>
 						</div>
 
 						<div ng-show='show_dwell_fct_actions_data'>
