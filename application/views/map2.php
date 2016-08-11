2ee34fd8e77dbd154d1c701ba0b62695a5044b3b
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-11 21:15:23 +0100

    Test

--- application/views/map2.php
+++ application/views/map2.php
@@ -84,10 +84,16 @@
 		<div class="container-fluid">
 
 			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 				<a class="navbar-brand" href="#">Hello {{ profile.first_name }}</a>
 			</div>
 
-			<div class="collapse navbar-collapse">
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<?php if($is_admin == 1): ?>
 		 				<li><p class="navbar-text">Fossils loaded: {{nbfossils}}</p></li>
