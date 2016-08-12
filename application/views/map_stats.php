239b106c3583993b6a72e6e78b99fb8d5f766d90
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-12 11:09:29 +0100

    T

--- application/views/map_stats.php
+++ application/views/map_stats.php
@@ -50,10 +50,16 @@
 		<div class="container-fluid">
 
 			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 				<a class="navbar-brand" href="#">Admin Stats</a>
 			</div>
 
-			<div class="collapse navbar-collapse">
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse">
 				<ul class="nav navbar-nav">
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
