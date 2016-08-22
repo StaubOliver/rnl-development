11cf7125ed44c4f45a41597bed853ffa147a1406
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-23 00:41:01 +0100

    T

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1542,6 +1542,9 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 
 			map.addListener("click", function(event){
 				console.log(event.latLng.lat());
+				$scope.newLat = event.latLng.lat();
+			    $scope.newLng = event.latLng.lng();
+			    console.log($scope.newLng);
 				if (marker != null){
 					marker.setMap(null);
 					marker = null;
@@ -1551,8 +1554,7 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 			        map: map, 
 			        icon: pin_selected
 			    });
-			    $scope.newLat = event.latLng.lat();
-			    $scope.newLng = event.latLng.lng();
+			    
 			});
 
 		}
