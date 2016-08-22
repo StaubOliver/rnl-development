cd751a7c32b7ceb3b4c168bf642918b9bf136c7c
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-22 19:44:33 +0100

    T

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -79,15 +79,15 @@
 
 				<div class='col-md-10 col-md-offset-1 no-padding'>
 
-					<div class='col-md-4 admin-tabs-col admin-tabs-left'>
+					<div class='col-md-4 admin-tabs-col no-padding admin-tabs-left'>
 						<h4 class='admin-tabs' ng-class="{'admin-tabs-active':selected_tab == 1}" ng-click='change_tab(1)'>Contributions</h4>
 					</div>
 
-					<div class='col-md-4 admin-tabs-col admin-tabs-middle'>
+					<div class='col-md-4 admin-tabs-col no-padding admin-tabs-middle'>
 						<h4 class='admin-tabs' ng-class="{'admin-tabs-active':selected_tab == 2}" ng-click='change_tab(2)'>Coordinates</h4>
 					</div>
 
-					<div class='col-md-4 admin-tabs-col admin-tabs-right'>
+					<div class='col-md-4 admin-tabs-col no-padding admin-tabs-right'>
 						<h4 class='admin-tabs' ng-class="{'admin-tabs-active':selected_tab == 3}" ng-click='change_tab(3)'>Collectors</h4>
 					</div>
 
