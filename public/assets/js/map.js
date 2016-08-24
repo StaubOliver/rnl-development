c32b036eb9a9e8a06f1947cb62f4501eaeb1b0eb
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-24 01:10:06 +0100

    T

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1654,7 +1654,7 @@ var map_stats = angular.module('map_stats', []).controller('admin_map_stats', fu
 
 					histogram: 
 					{ 
-						bucketSize: 10,
+						bucketSize: 20,
 						hideBucketItems: true, 
 						maxNumBuckets: 80,
 						minValue: 0,
@@ -1672,7 +1672,8 @@ var map_stats = angular.module('map_stats', []).controller('admin_map_stats', fu
 					},
 
 					hAxis: {
-						ticks: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+						ticks: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+						viewWindowMode: 'pretty'
 					}
 
 					/*vAxis: {
