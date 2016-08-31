523759862992aadcce6bae52fd113ae6935a9631
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-09-01 00:59:47 +0100

    t

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1645,7 +1645,7 @@ var map_admin = angular.module('map_admin', []).controller('admin_map_feedbacks'
 	        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 		    	
 		}).success(function(data, status, headers, config) {
-			
+			changeFossil();
 
 		}).error(function(data, status, headers, config){
 
