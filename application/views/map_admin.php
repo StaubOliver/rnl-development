f26ba9e9d803fc1a5844624377c1e50ba818a21c
mbaxros3 <olivier.staub@postgrad.manchester.ac.uk>
2016-08-31 23:31:29 +0100

    t

--- application/views/map_admin.php
+++ application/views/map_admin.php
@@ -570,7 +570,7 @@
 						<div class='row'>
 
 							<div class='col-md-3'>
-								<h5>{{list_fossils.has_coordinates.length + list_fossils.conversion_failed/length}} fossils</h5>
+								<h5>{{ (list_fossils.has_coordinates.length|number) + (list_fossils.conversion_failed/length|number) }} fossils</h5>
 							</div>
 
 							<div class='col-md-3'>
